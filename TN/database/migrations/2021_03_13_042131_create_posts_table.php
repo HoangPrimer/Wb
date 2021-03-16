ccc48e6579a850ece016ddba3899889091608508
HoangPrimer <nphoang2ko@gmail.com>
2021-03-16 20:41:41 +0700

    lolol

---
+++ TN/database/migrations/2021_03_13_042131_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('directory_id');
+            $table->integer('user_id');
+            $table->string('name_product');
+            $table->string('content');
+            $table->integer('price');
+            $table->string('public')->default('private');
+            $table->boolean('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
